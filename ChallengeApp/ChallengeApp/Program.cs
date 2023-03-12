fd1378ea2918d79d9530db556f565cd08896ec7b
emixx <emixx@o2.pl>
2023-03-12 20:55:16 +0100

    dzień 4 zadanie

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+var name = "Ewa";
+var age = 28;
+var sex = "Kobieta";
+
+
+
+if (age < 30 && sex == "Kobieta")
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" &&  age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex != "Kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Meżczyzna");
+}
