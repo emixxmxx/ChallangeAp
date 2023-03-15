e4f4c8f005d273f76f5cb12292258d34d5c87df4
emixx <emixx@o2.pl>
2023-03-15 23:05:16 +0100

    zadanie dzień 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,52 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> score1 = new List<int>();
+        private List<int> score2 = new List<int>();
+        private List<int> score3 = new List<int>();
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        public int R1
+        {
+            get
+            {
+                return this.score1.Sum();
+            }
+        }
+        public int R2
+        {
+            get
+            {
+                return this.score2.Sum();
+            }
+        }
+        public int R3
+        {
+            get
+            {
+                return this.score3.Sum();
+            }
+        }
+        public void AddScore1(int number)
+        {
+            this.score1.Add(number);
+        }
+        public void AddScore2(int number1)
+        {
+            this.score2.Add(number1);
+        }
+        public void AddScore3(int number2)
+        {
+            this.score3.Add(number2);
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,72 +1,40 @@
-﻿using System.ComponentModel.Design;
+﻿using ChallengeApp;
 
-int number = 389777;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToCharArray();
+Employee worker1 = new Employee("Adrian", "Puchacki", 21);
+Employee worker2 = new Employee("Wojciech", "Puczyk", 20);
+Employee worker3 = new Employee("Waldemar", "Walasik", 22);
 
-int c0 = 0;
-int c1 = 0;
-int c2 = 0;
-int c3 = 0;
-int c4 = 0;
-int c5 = 0; 
-int c6 = 0; 
-int c7 = 0;
-int c8 = 0;
-int c9 = 0;
+worker1.AddScore1(1);
+worker1.AddScore1(2);
+worker1.AddScore1(2);
+worker1.AddScore1(1);
+worker1.AddScore1(3);
+worker2.AddScore2(4);
+worker2.AddScore2(5);
+worker2.AddScore2(5);
+worker2.AddScore2(6);
+worker2.AddScore2(3);
+worker3.AddScore3(7);
+worker3.AddScore3(9);
+worker3.AddScore3(8);
+worker3.AddScore3(7);
+worker3.AddScore3(10);
+var R1 = worker1.R1;
+var R2 = worker2.R2;
+var R3 = worker3.R3;
+var name1 = worker1.Name;
+var name2 = worker2.Name;
+var name3 = worker3.Name;
+var surname1 = worker1.Surname;
+var surname2 = worker2.Surname;
+var surname3 = worker3.Surname;
+var age1 = worker1.Age;
+var age2 = worker2.Age;
+var age3 = worker3.Age;
 
-
-foreach (char let in letters)
-{
-    if (let == '0')
-    {
-        c0++;
-    }
-    else if (let == '1')
-    {
-        c1++;
-    }
-    else if (let == '2')
-    {
-        c2++;
-    }
-    else if (let == '3')
-    {
-        c3++;
-    }
-    else if (let == '4')
-    {
-        c4++;
-    }
-    else if (let == '5')
-    {
-        c5++;
-    }
-    else if (let == '6')
-    {
-        c6++;
-    }
-    else if (let == '7')
-    {
-        c7++;
-    }
-    else if (let == '8')
-    {
-        c8++;
-    }
-    else if (let == '9')
-    {
-        c9++;
-    }
-}
-
-Console.WriteLine("0 ==> " + c0);
-Console.WriteLine("1 ==> " + c1);
-Console.WriteLine("2 ==> " + c2);
-Console.WriteLine("3 ==> " + c3);
-Console.WriteLine("4 ==> " + c4);
-Console.WriteLine("5 ==> " + c5);
-Console.WriteLine("6 ==> " + c6);
-Console.WriteLine("7 ==> " + c7);
-Console.WriteLine("8 ==> " + c8);
-Console.WriteLine("9 ==> " + c9);
+if ((R1 >= R2) && (R1 >= R3))
+    Console.WriteLine("Pracownik z najwyższą oceną to: "+name1+" "+surname1+", wiek: "+age1+" ocena - "+R1);
+else if ((R2 >= R1) && (R2 >= R3))
+    Console.WriteLine("Pracownik z najwyższą oceną to: "+name2+" "+surname2+", wiek: "+age2+" ocena - "+R2);
+else if ((R3 >= R1) && (R3 >= R2))
+    Console.WriteLine("Pracownik z najwyższą oceną to: "+name3+" "+surname3+", wiek: "+age3+" ocena - "+R3);
