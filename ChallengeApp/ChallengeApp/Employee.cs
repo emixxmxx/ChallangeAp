8f0d4ae20010e250195f4c640c29e45cda28060b
emixx <emixx@o2.pl>
2023-03-23 18:39:54 +0100

    Zaanie Dzień 7

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -2,9 +2,9 @@
 {
     public class Employee
     {
-        private List<int> score = new List<int>();
+        private List<int> score = new List<int>(); //lista zbierająca punkty
 
-        public Employee(string name, string surname, int age)
+        public Employee(string name, string surname, int age) // kontruktor z paramentrem pozwala tworzyć osobnych uzytkownków
         {
             this.Name = name;
             this.Surname = surname;
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -2,42 +2,45 @@
 
 Employee worker1 = new Employee("Adrian", "Puchacki", 21);
 Employee worker2 = new Employee("Wojciech", "Puczyk", 20);
-Employee worker3 = new Employee("Waldemar", "Walasik", 22);
+Employee worker3 = new Employee("Wardemar", "Walasik", 22);
+
 
 worker1.AddScore(1);
-worker1.AddScore(2);
+worker1.AddScore(7);
 worker1.AddScore(2);
 worker1.AddScore(1);
 worker1.AddScore(3);
-worker2.AddScore(4);
-worker2.AddScore(5);
-worker2.AddScore(5);
-worker2.AddScore(6);
-worker2.AddScore(3);
+worker2.AddScore(0);
+worker2.AddScore(9);
+worker2.AddScore(9);
+worker2.AddScore(9);
+worker2.AddScore(8);
 worker3.AddScore(7);
-worker3.AddScore(9);
-worker3.AddScore(8);
-worker3.AddScore(7);
-worker3.AddScore(10);
+worker3.AddScore(2);
+worker3.AddScore(3);
+worker3.AddScore(1);
+worker3.AddScore(0);
 
 
 List<Employee> employees = new List<Employee>()
 {
-    worker1, worker2, worker3
+    worker1, worker2
 };
+
 int maxResult = -1;
 Employee workerWithMaxResult = null;
 
-foreach (Employee worker in employees)
+foreach (var worker in employees)
 {
     if (worker.Result > maxResult)
     {
+        maxResult = worker.Result;
         workerWithMaxResult = worker;
     }
 }
 
-Console.WriteLine
-    ("Pracownik z najwyższą oceną to: "+ 
-    workerWithMaxResult.Name+" "+ workerWithMaxResult.Surname
-    + ", wiek: "+ workerWithMaxResult.Age+", ocena: " + workerWithMaxResult.Result);
-   
+
+Console.WriteLine("Pracownik "+workerWithMaxResult.Name+ " " + workerWithMaxResult.Surname);
+
+Console.WriteLine("wiek: "+workerWithMaxResult.Age);
+Console.WriteLine("otrzymał: " + workerWithMaxResult.Result+" punktów");
